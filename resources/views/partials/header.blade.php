ed037c53c16445a85eb66f2eb1bfb95532df36d9
federico filella <federico.filella@gmail.com>
2020-05-05 18:55:31 +0200

    init

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <img src="{{asset('img/logo.png')}}" alt="La Molisana">
+    <nav>
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="{{route('prodotti')}}">Prodotti</a>
+            </li>
+            <li>
+                <a href="#">Contatti</a>
+            </li>
+        </ul>
+    </nav>
+</header>
